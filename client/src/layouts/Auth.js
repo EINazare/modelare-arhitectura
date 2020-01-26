cc726dfed00deaa3c3e8142982520a093a7641d6
EINazare <emanuelioannazare@gmail.com>
2020-01-26 11:24:15 +0200

    feature(Auth.js): add redirect to login

--- client/src/layouts/Auth.js
+++ client/src/layouts/Auth.js
@@ -1,22 +1,5 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 // reactstrap components
 import { Container, Row } from "reactstrap";
 
@@ -75,7 +58,10 @@ class Auth extends React.Component {
           {/* Page content */}
           <Container className="mt--8 pb-5">
             <Row className="justify-content-center">
-              <Switch>{this.getRoutes(routes)}</Switch>
+              <Switch>
+                {this.getRoutes(routes)}
+                <Redirect from="/auth" to="/auth/login" />
+              </Switch>
             </Row>
           </Container>
         </div>
