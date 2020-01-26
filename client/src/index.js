b75ae5d09c00e0de3db5941d5bb06aadf6f8d6f8
EINazare <emanuelioannazare@gmail.com>
2020-01-26 14:44:48 +0200

    refactor(): move App.js inside index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./_helpers";
-import { App } from "./App";
 
 // setup fake backend
 // import { configureFakeBackend } from './_helpers';
@@ -13,9 +13,22 @@ import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
+import AdminLayout from "layouts/Admin";
+import TeacherLayout from "layouts/Teacher";
+import StudentLayout from "layouts/Student";
+import AuthLayout from "layouts/Auth";
+
 render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/admin" render={props => <AdminLayout {...props} />} />
+        <Route path="/teacher" render={props => <TeacherLayout {...props} />} />
+        <Route path="/student" render={props => <StudentLayout {...props} />} />
+        <Route path="/auth" render={props => <AuthLayout {...props} />} />
+        <Redirect from="/" to="/auth" />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("app")
 );
