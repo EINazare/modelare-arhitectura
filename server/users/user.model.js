265782a6f918bbe35a487b55609deb943ce850a2
EINazare <emanuelioannazare@gmail.com>
2020-01-26 15:26:59 +0200

    fix(): stop error on new run of server

--- server/users/user.model.js
+++ server/users/user.model.js
@@ -6,37 +6,63 @@ const schema = new Schema({
   hash: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  role: { type: String, enum: ["admin","teacher","student"] },
+  role: { type: String, enum: ["admin", "teacher", "student"] },
   createdDate: { type: Date, default: Date.now }
 });
 
 schema.set("toJSON", { virtuals: true });
 
-const User = mongoose.model("User", schema)
+const User = mongoose.model("User", schema);
 
-const user = new User({
+let user = new User({
   email: "manu@creative-tim.com",
   hash: "$2y$10$tAB3EsS.9fiRjll0.RmyM.y/wsq38a0yl8lJudxu4gv1Fm463kLVa",
   firstName: "Manu",
   lastName: "Admin",
   role: "admin"
-},{
+});
+
+user.save(err => {
+  if(err){
+    if (err.code === 11000){}
+    else {
+      throw err;
+    }
+  }
+});
+
+user = new User({
   email: "emanuelioannazare@gmail.com",
   hash: "$2y$10$tAB3EsS.9fiRjll0.RmyM.y/wsq38a0yl8lJudxu4gv1Fm463kLVa",
   firstName: "Manu",
   lastName: "Teacher",
   role: "teacher"
-},{
-  email: "emanuelioannazare@gmail.com",
+});
+
+user.save(err => {
+  if(err){
+    if (err.code === 11000){}
+    else {
+      throw err;
+    }
+  }
+});
+
+user = new User({
+  email: "emanuel-ioan.nazare@my.fmi.unibuc.ro",
   hash: "$2y$10$tAB3EsS.9fiRjll0.RmyM.y/wsq38a0yl8lJudxu4gv1Fm463kLVa",
   firstName: "Manu",
   lastName: "Student",
   role: "student"
-}
-)
+});
 
 user.save(err => {
-  if(err) throw err;
-})
+  if(err){
+    if (err.code === 11000){}
+    else {
+      throw err;
+    }
+  }
+});
 
 module.exports = User;
