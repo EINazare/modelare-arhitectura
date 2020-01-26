07112b88a24e2cc0396bb5777c7e3c1f00f93b97
EINazare <emanuelioannazare@gmail.com>
2020-01-26 11:34:59 +0200

    refactor(Login.js): make Login.js file the only login page

--- client/src/views/auth-layout/Login.js
+++ client/src/views/auth-layout/Login.js
@@ -1,24 +1,21 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import { Link } from "react-router-dom";
+
 // reactstrap components
-import { Button, Card, CardBody, Col } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Col
+} from "reactstrap";
 
 class Login extends React.Component {
   render() {
@@ -26,28 +23,41 @@ class Login extends React.Component {
       <>
         <Col lg="5" md="7">
           <Card className="bg-secondary shadow border-0">
+            <CardHeader className="bg-transparent">
+              <CardTitle className="text-muted text-center mt-2 mb-3">
+                Sign in with your credentials
+              </CardTitle>
+            </CardHeader>
             <CardBody className="px-lg-5 py-lg-5">
-              <div className="text-center">
-                <Link to="/auth/admin-login">
-                  <Button className="my-4" color="primary" type="button">
-                    Admin Login
-                  </Button>
-                </Link>
-              </div>
-              <div className="text-center">
-                <Link to="/auth/teacher-login">
-                  <Button className="my-4" color="primary" type="button">
-                    Teacher Login
-                  </Button>
-                </Link>
-              </div>
-              <div className="text-center">
-                <Link to="/auth/student-login">
-                  <Button className="my-4" color="primary" type="button">
-                    Student Login
-                  </Button>
-                </Link>
-              </div>
+              <Form role="form">
+                <FormGroup className="mb-3">
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Email" type="email" />
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Password" type="password" />
+                  </InputGroup>
+                </FormGroup>
+                <div className="text-center">
+                  <Link to="/admin/teachers">
+                    <Button className="my-4" color="primary" type="button">
+                      Sign in
+                    </Button>
+                  </Link>
+                </div>
+              </Form>
             </CardBody>
           </Card>
         </Col>
